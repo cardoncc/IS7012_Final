3167aedca8be9495f6c18e42445a5d097ad46199
Troyerkr <krobtroyer@gmail.com>
2021-10-01 11:59:39 -0400

    Added additional pages and finalized the models for scaffolding

--- Dibs/Dibs/Models/Attendee.cs
+++ Dibs/Dibs/Models/Attendee.cs
@@ -24,7 +24,7 @@ namespace Dibs.Models
         public bool IsAttending { get; set; }
 
         [StringLength(90)]
-        public string? Message { get; set; }
+        public string Message { get; set; }
 
 
 
