ae70bda3a14e256a2cf165a6d196a10c78a8cc51
Troyerkr <krobtroyer@gmail.com>
2021-10-02 19:06:31 -0400

    Not Working: added invite page
    The on post method is getting a null. probably has something to do with the [bindproperty] but I don't know how to look at the post request to know for sure

---
+++ Dibs/Dibs/InviteFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dibs
+{
+    public class InviteFormModel
+    {
+        public int MeetingId { get; set; }
+        public int?[] UserIds { get; set; }
+    }
+}
---
+++ Dibs/Dibs/Pages/Meetings/Invite.cshtml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Dibs.Data;
+using Dibs.Models;
+
+namespace Dibs.Pages.Meetings
+{
+    public class InviteModel : PageModel
+    {
+        private readonly Dibs.Data.DibsContext _context;
+
+        //connect to database
+        public InviteModel(Dibs.Data.DibsContext context)
+        {
+            _context = context;
+        }
+
+        //added page model attributes
+        public Meeting NewMeeting { get; set; }
+        public List<MeetingUser> UserList { get; set; }
+        [BindProperty]
+        public InviteFormModel Form { get; set; }
+
+        public IActionResult OnGet(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            //Find the meeting that was just created
+            NewMeeting = _context.Meeting.Find(id);
+            //Find all of the users
+            UserList = _context.MeetingUser.ToList();
+
+
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            NewMeeting = _context.Meeting.Find(id);
+            
+            foreach(int uid in Form.UserIds)
+            {
+                //Create an attendee reccord
+                var ma = new Attendee { Meeting = NewMeeting, MeetingUserId = uid };
+                _context.Attendee.Add(ma);
+            }
+
+            //_context.Attendee.Add(Attendee);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
